b5487ebfa114f794b09738e7c57181c7de584af5
mustafaerbay <mustafaerbay365@gmail.com>
2022-02-09 22:20:08 +0300

    Use eip association and scale up web instances

--- prod.tf
+++ prod.tf
@@ -41,6 +41,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+  count = 2
+
   ami           = "ami-03a0cd500c36e8f3c"
   instance_type = "t2.micro"
 
@@ -53,8 +55,12 @@ resource "aws_instance" "prod_web" {
   }
 }
 
+resource "aws_eip_association" "prod_web" {
+  instance_id   = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
+
 resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
 
   tags = {
     "Terraform" : "true"
